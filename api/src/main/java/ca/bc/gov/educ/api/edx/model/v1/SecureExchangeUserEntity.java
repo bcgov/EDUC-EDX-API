f8f8bab7e1c840487d4ad765ce9fddeb916225cd
harshit-kohli <harshit.kohli@gov.bc.ca>
2022-02-04 09:31:52 -0800

    EDX API Structure

---
+++ api/src/main/java/ca/bc/gov/educ/api/edx/model/v1/SecureExchangeUserEntity.java
@@ -0,0 +1,49 @@
+package ca.bc.gov.educ.api.edx.model.v1;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@Entity
+@Table(name = "SECURE_EXCHANGE_USER")
+@DynamicUpdate
+public class SecureExchangeUserEntity {
+  @Id
+  @GeneratedValue(generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator", parameters = {
+          @Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")})
+  @Column(name = "EDX_USER_ID", updatable = false, columnDefinition = "BINARY(16)")
+  UUID edxUserID;
+
+  @Column(name = "DIGITAL_IDENTITY_ID", updatable = false, columnDefinition = "BINARY(16)")
+  UUID digitalIdentityID;
+
+  @Column(name = "FIRST_NAME")
+  String firstName;
+
+  @Column(name = "LAST_NAME")
+  String lastName;
+
+  @Column(name = "CREATE_USER", updatable = false)
+  String createUser;
+
+  @PastOrPresent
+  @Column(name = "CREATE_DATE", updatable = false)
+  LocalDateTime createDate;
+
+  @Column(name = "update_user")
+  String updateUser;
+
+  @PastOrPresent
+  @Column(name = "update_date")
+  LocalDateTime updateDate;
+
+}
