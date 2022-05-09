2424bbc0aa453fe71599b8cf763c0fe08d9bb96d
sukanya.rath <sukanyarath1987@gmail.com>
2022-05-09 12:39:27 -0700

    EDX-21 - Adding Columns to tables

--- api/src/main/java/ca/bc/gov/educ/api/edx/struct/v1/EdxActivationCode.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/struct/v1/EdxActivationCode.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.List;
@@ -30,12 +32,16 @@ public class EdxActivationCode extends BaseRequest implements Serializable {
   private List<EdxActivationRole> edxActivationRoles;
 
   @Size(max = 255)
+  @NotNull(message = "First Name cannot be null")
   String firstName;
 
   @Size(max = 255)
+  @NotNull(message = "Last Name cannot be null")
   String lastName;
 
   @Size(max = 255)
+  @NotNull(message = "Email cannot be null")
+  @Email(message = "Email address should be a valid email address")
   String email;
 
 }
