e524b9b9dfb7e84d3b1e742e7ebe90c867863c28
sukanya.rath <sukanyarath1987@gmail.com>
2022-05-10 12:42:51 -0700

    EDX-21 | SCH - Activate EDX Account Adding unique key constraints on table

--- api/src/main/java/ca/bc/gov/educ/api/edx/model/v1/EdxUserSchoolEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/model/v1/EdxUserSchoolEntity.java
@@ -16,38 +16,38 @@ import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "EDX_USER_SCHOOL")
+@Table(name = "EDX_USER_SCHOOL", uniqueConstraints = {@UniqueConstraint(name = "EDX_USER_ID_MINCODE_UK", columnNames = {"edxUserID", "mincode"})})
 @DynamicUpdate
 public class EdxUserSchoolEntity {
-  @Id
-  @GeneratedValue(generator = "UUID")
-  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator", parameters = {
-          @Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")})
-  @Column(name = "EDX_USER_SCHOOL_ID", updatable = false, columnDefinition = "BINARY(16)")
-  UUID edxUserSchoolID;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator", parameters = {
+            @Parameter(name = "uuid_gen_strategy_class", value = "org.hibernate.id.uuid.CustomVersionOneStrategy")})
+    @Column(name = "EDX_USER_SCHOOL_ID", updatable = false, columnDefinition = "BINARY(16)")
+    UUID edxUserSchoolID;
 
-  @Column(name = "EDX_USER_ID", updatable = false, columnDefinition = "BINARY(16)")
-  UUID edxUserID;
+    @Column(name = "EDX_USER_ID", updatable = false, columnDefinition = "BINARY(16)")
+    UUID edxUserID;
 
-  @Column(name = "MINCODE")
-  String mincode;
+    @Column(name = "MINCODE")
+    String mincode;
 
-  @Column(name = "CREATE_USER", updatable = false)
-  String createUser;
+    @Column(name = "CREATE_USER", updatable = false)
+    String createUser;
 
-  @PastOrPresent
-  @Column(name = "CREATE_DATE", updatable = false)
-  LocalDateTime createDate;
+    @PastOrPresent
+    @Column(name = "CREATE_DATE", updatable = false)
+    LocalDateTime createDate;
 
-  @Column(name = "update_user")
-  String updateUser;
+    @Column(name = "update_user")
+    String updateUser;
 
-  @PastOrPresent
-  @Column(name = "update_date")
-  LocalDateTime updateDate;
+    @PastOrPresent
+    @Column(name = "update_date")
+    LocalDateTime updateDate;
 
-  @ToString.Exclude
-  @EqualsAndHashCode.Exclude
-  @OneToMany(mappedBy = "edxUserSchoolEntity", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true, targetEntity = EdxUserSchoolRoleEntity.class)
-  private Set<EdxUserSchoolRoleEntity> edxUserSchoolRoleEntities;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "edxUserSchoolEntity", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true, targetEntity = EdxUserSchoolRoleEntity.class)
+    private Set<EdxUserSchoolRoleEntity> edxUserSchoolRoleEntities;
 }
