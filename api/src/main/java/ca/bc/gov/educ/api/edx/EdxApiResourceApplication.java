a49261a00cba3a8ca68d7c3342eb1890232c99fc
sukanya.rath <sukanyarath1987@gmail.com>
2022-05-12 12:47:00 -0700

    EDX-21 | SCH - Activate EDX Account Some of the create and delete endpoints for EDX_USER, EDX_USER_SCHOOL and EDX_USER_SCHOOL_ROLE

--- api/src/main/java/ca/bc/gov/educ/api/edx/EdxApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/EdxApiResourceApplication.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,7 +21,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableCaching
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "1s")
@@ -51,7 +52,7 @@ public class EdxApiResourceApplication {
     public void configure(final WebSecurity web) {
       web.ignoring().antMatchers("/v3/api-docs/**",
               "/actuator/health", "/actuator/prometheus",
-              "/swagger-ui/**", "/health", "/api/**");
+              "/swagger-ui/**", "/health");
     }
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
