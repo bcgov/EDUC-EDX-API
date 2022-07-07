1790c94b5acecfb35ffd26eee219cdf9aa942280
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-07-07 15:52:58 -0700

    Update SecureExchangeStudentRepository.java

--- api/src/main/java/ca/bc/gov/educ/api/edx/repository/SecureExchangeStudentRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/repository/SecureExchangeStudentRepository.java
@@ -1,7 +1,14 @@
 package ca.bc.gov.educ.api.edx.repository;
 
-import ca.bc.gov.educ.api.edx.struct.v1.SecureExchangeStudent;
+import ca.bc.gov.educ.api.edx.model.v1.SecureExchangeEntity;
+import ca.bc.gov.educ.api.edx.model.v1.SecureExchangeStudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface SecureExchangeStudentRepository extends JpaRepository<SecureExchangeStudent, String> {
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SecureExchangeStudentRepository extends JpaRepository<SecureExchangeStudentEntity, UUID> {
+
 }
