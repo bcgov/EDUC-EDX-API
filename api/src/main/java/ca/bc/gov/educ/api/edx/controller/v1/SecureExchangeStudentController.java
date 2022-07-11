77a68735424e38440e75797efc835ec30551a469
sukanya.rath <sukanyarath1987@gmail.com>
2022-07-11 10:41:45 -0700

    EDX-179 | Removing unused imports

--- api/src/main/java/ca/bc/gov/educ/api/edx/controller/v1/SecureExchangeStudentController.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/controller/v1/SecureExchangeStudentController.java
@@ -1,7 +1,6 @@
 package ca.bc.gov.educ.api.edx.controller.v1;
 
 import ca.bc.gov.educ.api.edx.endpoint.v1.SecureExchangeStudentEndpoint;
-import ca.bc.gov.educ.api.edx.mappers.v1.SecureExchangeEntityMapper;
 import ca.bc.gov.educ.api.edx.service.v1.SecureExchangeStudentService;
 import ca.bc.gov.educ.api.edx.struct.v1.SecureExchange;
 import ca.bc.gov.educ.api.edx.struct.v1.SecureExchangeStudent;
