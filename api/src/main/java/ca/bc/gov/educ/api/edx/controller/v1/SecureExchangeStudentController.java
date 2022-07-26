55546dc04fe48d582d1a3330492c47c689b03390
arcshiftsolutions <arcshiftsolutions@gmail.com>
2022-07-26 13:40:13 -0700

    Updates to add context columns for the user.

--- api/src/main/java/ca/bc/gov/educ/api/edx/controller/v1/SecureExchangeStudentController.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/controller/v1/SecureExchangeStudentController.java
@@ -2,44 +2,66 @@ package ca.bc.gov.educ.api.edx.controller.v1;
 
 import ca.bc.gov.educ.api.edx.endpoint.v1.SecureExchangeStudentEndpoint;
 import ca.bc.gov.educ.api.edx.exception.EntityNotFoundException;
+import ca.bc.gov.educ.api.edx.exception.InvalidPayloadException;
 import ca.bc.gov.educ.api.edx.exception.NotFoundException;
+import ca.bc.gov.educ.api.edx.exception.errors.ApiError;
 import ca.bc.gov.educ.api.edx.service.v1.SecureExchangeStudentService;
 import ca.bc.gov.educ.api.edx.struct.v1.SecureExchange;
 import ca.bc.gov.educ.api.edx.struct.v1.SecureExchangeStudent;
+import ca.bc.gov.educ.api.edx.validator.SecureExchangeStudentValidator;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
 public class SecureExchangeStudentController implements SecureExchangeStudentEndpoint {
 
-    @Getter(AccessLevel.PRIVATE)
-    private final SecureExchangeStudentService studentService;
+  @Getter(AccessLevel.PRIVATE)
+  private final SecureExchangeStudentValidator payloadValidator;
 
-    @Autowired
-    public SecureExchangeStudentController(SecureExchangeStudentService studentService) {
-        this.studentService = studentService;
+  @Getter(AccessLevel.PRIVATE)
+  private final SecureExchangeStudentService studentService;
+
+  @Autowired
+  public SecureExchangeStudentController(SecureExchangeStudentValidator payloadValidator, SecureExchangeStudentService studentService) {
+    this.payloadValidator = payloadValidator;
+    this.studentService = studentService;
+  }
+
+  @Override
+  public SecureExchange addStudent(String secureExchangeId, SecureExchangeStudent secureExchangeStudent) throws NotFoundException, EntityNotFoundException {
+    validatePayload(secureExchangeStudent);
+    return this.getStudentService().addStudentToExchange(UUID.fromString(secureExchangeId), secureExchangeStudent);
+  }
+
+  @Override
+  public List<SecureExchangeStudent> getStudents(String secureExchangeId) {
+    return this.getStudentService().getStudentIDsFromExchange(UUID.fromString(secureExchangeId));
+  }
+
+  @Override
+  public ResponseEntity<Void> deleteStudent(String secureExchangeStudentId, String secureExchangeId) {
+    this.getStudentService().deleteStudentFromExchange(UUID.fromString(secureExchangeStudentId));
+    return ResponseEntity.noContent().build();
+  }
+
+  private void validatePayload(SecureExchangeStudent secureExchangeStudent) {
+    val validationResult = getPayloadValidator().validatePayload(secureExchangeStudent);
+    if (!validationResult.isEmpty()) {
+      ApiError error = ApiError.builder().timestamp(LocalDateTime.now()).message("Payload contains invalid data.").status(BAD_REQUEST).build();
+      error.addValidationErrors(validationResult);
+      throw new InvalidPayloadException(error);
     }
 
-    @Override
-    public SecureExchange addStudent(String secureExchangeId, SecureExchangeStudent secureExchangeStudent) throws NotFoundException, EntityNotFoundException {
-        return this.getStudentService().addStudentToExchange(UUID.fromString(secureExchangeId), secureExchangeStudent);
-    }
-
-    @Override
-    public List<SecureExchangeStudent> getStudents(String secureExchangeId) {
-        return this.getStudentService().getStudentIDsFromExchange(UUID.fromString(secureExchangeId));
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteStudent(String secureExchangeStudentId, String secureExchangeId) {
-        this.getStudentService().deleteStudentFromExchange(UUID.fromString(secureExchangeStudentId));
-        return ResponseEntity.noContent().build();
-    }
+  }
 
 }
---
+++ api/src/main/java/ca/bc/gov/educ/api/edx/validator/SecureExchangeStudentValidator.java
@@ -0,0 +1,26 @@
+package ca.bc.gov.educ.api.edx.validator;
+
+import ca.bc.gov.educ.api.edx.struct.v1.SecureExchangeStudent;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class SecureExchangeStudentValidator {
+
+  public List<FieldError> validatePayload(final SecureExchangeStudent student) {
+    final List<FieldError> apiValidationErrors = new ArrayList<>();
+    if (StringUtils.isEmpty(student.getStaffUserIdentifier()) && student.getEdxUserID() == null) {
+      apiValidationErrors.add(createFieldError("staffUserIdentifier", student.getStaffUserIdentifier(), "staffUserIdentifier and edxUserID are both null, one must be provided"));
+    }
+
+    return apiValidationErrors;
+  }
+
+  private FieldError createFieldError(String fieldName, Object rejectedValue, String message) {
+    return new FieldError("secureExchange", fieldName, rejectedValue, false, null, null, message);
+  }
+}
