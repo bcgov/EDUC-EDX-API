71a884ab9e34d3bb9a0ba0bc6336375f347cb5ae
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2022-07-28 08:20:43 -0700

    Moved method to EdxUserSchoolRepository.

--- api/src/main/java/ca/bc/gov/educ/api/edx/repository/EdxUserRepository.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/repository/EdxUserRepository.java
@@ -2,9 +2,6 @@ package ca.bc.gov.educ.api.edx.repository;
 
 import ca.bc.gov.educ.api.edx.model.v1.EdxUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,10 +12,4 @@ public interface EdxUserRepository extends JpaRepository<EdxUserEntity, UUID>, E
 
   List<EdxUserEntity> findEdxUserEntitiesByDigitalIdentityID(UUID digitalIdentityID);
 
-  @Modifying
-  @Query(
-    value = "DELETE FROM EDX_USER_SCHOOL WHERE EDX_USER_SCHOOL_ID = (:edxUserSchoolID)\\:\\:uuid",
-    nativeQuery = true
-  )
-  void deleteEdxUserSchool(@Param("edxUserSchoolID") String school);
 }
