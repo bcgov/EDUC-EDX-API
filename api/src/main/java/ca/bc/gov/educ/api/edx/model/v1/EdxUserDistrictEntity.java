b872ebfd8d7b885aa21fea6d9eee14735c8bb88d
sukanya.rath <sukanyarath1987@gmail.com>
2022-09-07 20:38:59 -0700

    EDX-455 | CRUD API for EdxUserDistrict and EdxUserDistrictRoles

--- api/src/main/java/ca/bc/gov/educ/api/edx/model/v1/EdxUserDistrictEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/model/v1/EdxUserDistrictEntity.java
@@ -37,7 +37,7 @@ public class EdxUserDistrictEntity {
   /**
    * The Edx user entity.
    */
-  @ManyToOne(targetEntity = EdxUserEntity.class)
+  @ManyToOne(optional = false,targetEntity = EdxUserEntity.class)
   @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
   @JoinColumn(name = "EDX_USER_ID", referencedColumnName = "EDX_USER_ID")
   EdxUserEntity edxUserEntity;
