50e825e84411acb7bda6de99c31c71cc6eefcd3e
SodhiA1 <38086281+SodhiA1@users.noreply.github.com>
2023-03-13 10:22:11 -0700

    updates

---
+++ api/src/main/java/ca/bc/gov/educ/api/edx/orchestrator/MoveSchoolOrchestrator.java
@@ -0,0 +1,96 @@
+package ca.bc.gov.educ.api.edx.orchestrator;
+
+import ca.bc.gov.educ.api.edx.exception.APIServiceException;
+import ca.bc.gov.educ.api.edx.messaging.MessagePublisher;
+import ca.bc.gov.educ.api.edx.model.v1.SagaEntity;
+import ca.bc.gov.educ.api.edx.model.v1.SagaEventStatesEntity;
+import ca.bc.gov.educ.api.edx.orchestrator.base.BaseOrchestrator;
+import ca.bc.gov.educ.api.edx.service.v1.MoveSchoolOrchestratorService;
+import ca.bc.gov.educ.api.edx.service.v1.SagaService;
+import ca.bc.gov.educ.api.edx.struct.v1.Event;
+import ca.bc.gov.educ.api.edx.struct.v1.MoveSchoolSagaData;
+import ca.bc.gov.educ.api.edx.struct.v1.School;
+import ca.bc.gov.educ.api.edx.utils.JsonUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+import static ca.bc.gov.educ.api.edx.constants.EventOutcome.*;
+import static ca.bc.gov.educ.api.edx.constants.EventType.*;
+import static ca.bc.gov.educ.api.edx.constants.SagaEnum.MOVE_SCHOOL_SAGA;
+import static ca.bc.gov.educ.api.edx.constants.TopicsEnum.EDX_API_TOPIC;
+import static lombok.AccessLevel.PRIVATE;
+
+@Component
+@Slf4j
+public class MoveSchoolOrchestrator extends BaseOrchestrator<MoveSchoolSagaData> {
+
+    @Getter(PRIVATE)
+    private final MoveSchoolOrchestratorService moveSchoolOrchestratorService;
+    /**
+     * Instantiates a new Base orchestrator.
+     *
+     * @param sagaService                   the saga service
+     * @param messagePublisher              the message publisher
+     * @param moveSchoolOrchestratorService
+     */
+    protected MoveSchoolOrchestrator(SagaService sagaService, MessagePublisher messagePublisher, MoveSchoolOrchestratorService moveSchoolOrchestratorService) {
+        super(sagaService, messagePublisher, MoveSchoolSagaData.class, MOVE_SCHOOL_SAGA.toString(), EDX_API_TOPIC.toString());
+        this.moveSchoolOrchestratorService = moveSchoolOrchestratorService;
+    }
+
+    @Override
+    public void populateStepsToExecuteMap() {
+        this.stepBuilder()
+          .begin(CREATE_SCHOOL, this::checkIfSchoolNumberIsAvailableInDistrict)
+          .step(CREATE_SCHOOL, SCHOOL_CREATED, UPDATE_SCHOOL, this::updateExistingSchool)
+          .step(UPDATE_SCHOOL, SCHOOL_UPDATED, MOVE_USERS_TO_NEW_SCHOOL, this::moveUsersToNewSchool)
+          .end(MOVE_USERS_TO_NEW_SCHOOL, SCHOOL_MOVED);
+    }
+
+    private void checkIfSchoolNumberIsAvailableInDistrict(Event event, SagaEntity saga, MoveSchoolSagaData moveSchoolSagaData) throws JsonProcessingException{
+        final SagaEventStatesEntity eventStates = this.createEventState(saga, event.getEventType(), event.getEventOutcome(), event.getEventPayload());
+        saga.setSagaState(CREATE_SCHOOL.toString());
+        this.getSagaService().updateAttachedSagaWithEvents(saga, eventStates);
+        boolean exists = getMoveSchoolOrchestratorService().findSchoolNumberInDistrict(moveSchoolSagaData.getSchool().getSchoolNumber(), moveSchoolSagaData.getSchool().getDistrictId());
+        getMoveSchoolOrchestratorService().createNewSchool(moveSchoolSagaData, saga, exists);
+
+        final Event nextEvent = Event.builder().sagaId(saga.getSagaId())
+                .eventType(CREATE_SCHOOL).eventOutcome(SCHOOL_CREATED)
+                .eventPayload(JsonUtil.getJsonStringFromObject(moveSchoolSagaData))
+                .build();
+        this.postMessageToTopic(this.getTopicToSubscribe(), nextEvent);
+        log.info("message sent to EDX_API_TOPIC for CREATE_SCHOOL Event.");
+    }
+
+    private void updateExistingSchool(Event event, SagaEntity saga, MoveSchoolSagaData moveSchoolSagaData) throws JsonProcessingException{
+        final SagaEventStatesEntity eventStates = this.createEventState(saga, event.getEventType(), event.getEventOutcome(), event.getEventPayload());
+        saga.setSagaState(UPDATE_SCHOOL.toString());
+        this.getSagaService().updateAttachedSagaWithEvents(saga, eventStates);
+        getMoveSchoolOrchestratorService().updateSchool(moveSchoolSagaData, saga);
+        final Event nextEvent = Event.builder().sagaId(saga.getSagaId())
+                .eventType(UPDATE_SCHOOL).eventOutcome(SCHOOL_UPDATED)
+                .eventPayload(JsonUtil.getJsonStringFromObject(moveSchoolSagaData))
+                .build();
+        this.postMessageToTopic(this.getTopicToSubscribe(), nextEvent);
+        log.info("message sent to EDX_API_TOPIC for UPDATE_EXISTING_SCHOOL Event.");
+    }
+
+    private void moveUsersToNewSchool(Event event, SagaEntity saga, MoveSchoolSagaData moveSchoolSagaData) throws JsonProcessingException{
+        final SagaEventStatesEntity eventStates = this.createEventState(saga, event.getEventType(), event.getEventOutcome(), event.getEventPayload());
+        saga.setSagaState(MOVE_USERS_TO_NEW_SCHOOL.toString());
+        this.getSagaService().updateAttachedSagaWithEvents(saga, eventStates);
+
+        getMoveSchoolOrchestratorService().moveUsersToNewSchool(moveSchoolSagaData, saga);
+
+        final Event nextEvent = Event.builder().sagaId(saga.getSagaId())
+                .eventType(MOVE_USERS_TO_NEW_SCHOOL).eventOutcome(SCHOOL_MOVED)
+                .eventPayload(JsonUtil.getJsonStringFromObject(moveSchoolSagaData))
+                .build();
+        this.postMessageToTopic(this.getTopicToSubscribe(), nextEvent);
+        log.info("message sent to EDX_API_TOPIC for MOVE_USERS_TO_NEW_SCHOOL Event.");
+    }
+}
---
+++ api/src/main/java/ca/bc/gov/educ/api/edx/service/v1/MoveSchoolOrchestratorService.java
@@ -0,0 +1,146 @@
+package ca.bc.gov.educ.api.edx.service.v1;
+
+import ca.bc.gov.educ.api.edx.exception.SagaRuntimeException;
+import ca.bc.gov.educ.api.edx.mappers.v1.EdxUserMapper;
+import ca.bc.gov.educ.api.edx.mappers.v1.EdxUserSchoolMapper;
+import ca.bc.gov.educ.api.edx.model.v1.SagaEntity;
+import ca.bc.gov.educ.api.edx.rest.RestUtils;
+import ca.bc.gov.educ.api.edx.struct.v1.*;
+import ca.bc.gov.educ.api.edx.utils.JsonUtil;
+import ca.bc.gov.educ.api.edx.utils.RequestUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class MoveSchoolOrchestratorService {
+
+    private final RestUtils restUtils;
+
+    protected final SagaService sagaService;
+
+    private static final EdxUserMapper userMapper = EdxUserMapper.mapper;
+
+    private static final EdxUserSchoolMapper USER_SCHOOL_MAPPER = EdxUserSchoolMapper.mapper;
+
+    @Getter(AccessLevel.PRIVATE)
+    private final EdxUsersService service;
+
+    public MoveSchoolOrchestratorService(RestUtils restUtils, SagaService sagaService, EdxUsersService service) {
+        this.restUtils = restUtils;
+        this.sagaService = sagaService;
+        this.service = service;
+    }
+
+    public boolean findSchoolNumberInDistrict(String schoolNumber, String districtId)  {
+         List<School> schools = restUtils.getSchoolNumberInDistrict(UUID.randomUUID(), schoolNumber, districtId);
+         return !schools.isEmpty();
+    }
+
+    public School createNewSchool(MoveSchoolSagaData moveSchoolSagaData, SagaEntity saga, boolean hasSchoolNumber) {
+        School createSchoolObj = createSchoolObject(moveSchoolSagaData, hasSchoolNumber);
+        School school  = restUtils.createSchool(UUID.randomUUID(), createSchoolObj);
+        if(school == null) {
+            throw new SagaRuntimeException("Create School Failed for: " + moveSchoolSagaData.getSchool().getSchoolNumber() + "," + moveSchoolSagaData.getSchool().getDisplayName());
+        }
+        moveSchoolSagaData.setNewSchoolId(school.getSchoolId());
+        moveSchoolSagaData.setNewSchoolNumber(school.getSchoolNumber());
+        try {
+           updateSagaDataInternal(moveSchoolSagaData, saga);
+         } catch (JsonProcessingException e) {
+          throw new SagaRuntimeException(e);
+        }
+        return school;
+    }
+
+    public School updateSchool(MoveSchoolSagaData moveSchoolSagaData, SagaEntity saga) {
+        List<School> schools = restUtils.getSchoolById(UUID.randomUUID(), moveSchoolSagaData.getSchool().getSchoolId());
+        if(schools.isEmpty()) {
+            throw new SagaRuntimeException("Update failed: School" + moveSchoolSagaData.getSchool().getSchoolId() + " does not exist.");
+        }
+
+        if(schools.size() > 1) {
+            throw new SagaRuntimeException("Update failed: Multiple matches found for: " + moveSchoolSagaData.getSchool().getSchoolId());
+        }
+        School schoolToUpdate = schools.get(0);
+        schoolToUpdate.setClosedDate(moveSchoolSagaData.getMoveDate());
+        RequestUtil.setAuditColumnsForUpdate(schoolToUpdate);
+        School school  = restUtils.updateSchool(UUID.randomUUID(), schoolToUpdate);
+        try {
+            if(school != null) {
+                updateSagaDataInternal(moveSchoolSagaData, saga);
+            }
+        } catch (JsonProcessingException e) {
+            throw new SagaRuntimeException(e);
+        }
+        return school;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void moveUsersToNewSchool(MoveSchoolSagaData moveSchoolSagaData, SagaEntity saga) {
+        List<EdxUser> userEntities =
+                getService().findEdxUsers(Optional.empty(), Optional.of(UUID.fromString(moveSchoolSagaData.getSchool().getSchoolId())),
+                        null, null, Optional.empty())
+                        .stream()
+                        .map(userMapper::toStructure).collect(Collectors.toList());
+
+        List<EdxUserSchool> userSchoolEntity = userEntities.stream().flatMap(edxUser -> edxUser.getEdxUserSchools().stream()).collect(Collectors.toList());
+        List<EdxUserSchool> matchedSchoolEntity = userSchoolEntity.stream().filter(edxUserSchool -> edxUserSchool.getSchoolID().equals(UUID.fromString(moveSchoolSagaData.getSchool().getSchoolId()))).collect(Collectors.toList());
+
+
+        for(EdxUserSchool schoolEntity: matchedSchoolEntity) {
+            getService().moveEdxUsersToNewSchool(UUID.fromString(schoolEntity.getEdxUserID()), USER_SCHOOL_MAPPER.toModel(schoolEntity), UUID.fromString(moveSchoolSagaData.getNewSchoolId()));
+        }
+
+        try {
+            updateSagaDataInternal(moveSchoolSagaData, saga);
+        } catch (JsonProcessingException e) {
+            throw new SagaRuntimeException(e);
+        }
+    }
+
+    private void updateSagaDataInternal(MoveSchoolSagaData moveSchoolSagaData, SagaEntity sagaEntity) throws JsonProcessingException {
+        sagaEntity.setSchoolID(UUID.fromString(moveSchoolSagaData.getSchool().getSchoolId()));
+        sagaEntity.setPayload(JsonUtil.getJsonStringFromObject(moveSchoolSagaData)); // update the payload which will be updated in DB.
+        this.sagaService.updateSagaRecord(sagaEntity); // save updated payload to DB again.
+    }
+
+    private School createSchoolObject(MoveSchoolSagaData moveSchoolSagaData, boolean hasSchoolNumber) {
+        School createSchool = moveSchoolSagaData.getSchool();
+        RequestUtil.setAuditColumnsForCreate(createSchool);
+
+        createSchool.setSchoolId(null);
+        createSchool.setNotes(null);
+        createSchool.setMincode(null);
+        createSchool.setOpenedDate(moveSchoolSagaData.getMoveDate());
+
+        if(!createSchool.getGrades().isEmpty()) {
+            createSchool.getGrades().stream().forEach(grade -> {
+                grade.setSchoolId(null);
+                grade.setSchoolGradeId(null);
+            });
+        }
+
+        if(!createSchool.getNeighborhoodLearning().isEmpty()) {
+            createSchool.getNeighborhoodLearning().stream().forEach(grade -> {
+                grade.setNeighborhoodLearningId(null);
+                grade.setSchoolId(null);
+            });
+        }
+
+        if(hasSchoolNumber) {
+            createSchool.setSchoolNumber(null);
+        }
+        return createSchool;
+    }
+}
