0dd71d1075954c7e96e484590c8491ac25f2230b
arcshiftsolutions <arcshiftsolutions@gmail.com>
2023-09-29 15:30:34 -0700

    Removed unused attribute

--- api/src/main/java/ca/bc/gov/educ/api/edx/struct/v1/EdxSchool.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/struct/v1/EdxSchool.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class EdxSchool implements Serializable {
   private static final long serialVersionUID = 583620260139143932L;
 
-  String schoolName;
   String schoolID;
 
   private List<EdxUser> edxUsers;
