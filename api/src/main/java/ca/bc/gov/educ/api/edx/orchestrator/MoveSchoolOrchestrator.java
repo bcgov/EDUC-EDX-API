2bfd8b4ee3e8d7378883593b53e3d668563ab73d
Marco Villeneuve <arcshiftsolutions@gmail.com>
2023-10-23 15:11:05 -0700

    Small log update

--- api/src/main/java/ca/bc/gov/educ/api/edx/orchestrator/MoveSchoolOrchestrator.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/orchestrator/MoveSchoolOrchestrator.java
@@ -80,10 +80,10 @@ public class MoveSchoolOrchestrator extends BaseOrchestrator<MoveSchoolData> {
         MoveSchoolData moveSchoolDataFromEvent = objectMapper.readValue(event.getEventPayload(), MoveSchoolData.class);
 
         if(!getMoveSchoolOrchestratorService().hasCopiedUsersAlready(moveSchoolDataFromEvent)) {
-            log.info("Copying school users to new school ID :: {}", moveSchoolData.getToSchool().getSchoolId());
+            log.info("Copying school users to new school ID :: {}", moveSchoolDataFromEvent.getToSchool().getSchoolId());
             getMoveSchoolOrchestratorService().copyUsersToNewSchool(moveSchoolDataFromEvent);
         }else{
-            log.info("School users were already copied to new school ID :: {}", moveSchoolData.getToSchool().getSchoolId());
+            log.info("School users were already copied to new school ID :: {}", moveSchoolDataFromEvent.getToSchool().getSchoolId());
         }
 
         final Event nextEvent = Event.builder().sagaId(saga.getSagaId())
