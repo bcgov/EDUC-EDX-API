dd467a151250fcba86759c07b5e91a950e2c8ce8
mightycox <j.acox@hotmail.com>
2023-11-02 13:20:23 -0700

    EDX-1950 - Updates edx roles, labels and descriptions

--- api/src/main/resources/db/migration/V1.0.38__EDX_API.sql
+++ api/src/main/resources/db/migration/V1.0.38__EDX_API.sql
@@ -1,23 +1,27 @@
 UPDATE EDX_ROLE
-SET LABEL = 'School Manager', DESCRIPTION = 'Can edit the details and contacts for the school.'
+SET LABEL = 'School Info Administrator', DESCRIPTION = 'Can edit school details and contacts.'
 WHERE EDX_ROLE_CODE = 'EDX_EDIT_SCHOOL';
 
 UPDATE EDX_ROLE
-SET LABEL = 'District Manager', DESCRIPTION = 'Can edit the details and contacts for the district and all schools in the district.'
+SET LABEL = 'District Info Administrator', DESCRIPTION = 'Can edit school and district details and contacts.'
 WHERE EDX_ROLE_CODE = 'EDX_EDIT_DISTRICT';
 
 UPDATE EDX_ROLE
-SET DESCRIPTION = 'Has access to all EDX features, including managing EDX users for the district and all schools in the district.'
+SET LABEL = 'EDX District Account Manager', DESCRIPTION = 'Can manage the school and district''s EDX users.'
 WHERE EDX_ROLE_CODE = 'EDX_DISTRICT_ADMIN';
 
 UPDATE EDX_ROLE
-SET DESCRIPTION = 'Has access to all EDX features, including managing EDX users for the school.'
+SET LABEL = 'EDX School Account Manager', DESCRIPTION = 'Can manage the school''s EDX users.'
 WHERE EDX_ROLE_CODE = 'EDX_SCHOOL_ADMIN';
 
 UPDATE EDX_ROLE
-SET LABEL = 'Secure Messaging', DESCRIPTION = 'Can send and receive messages through the Secure Messaging Inbox.'
+SET LABEL = 'Secure Messaging', DESCRIPTION = 'Can access the school''s Secure Messaging Inbox.'
 WHERE EDX_ROLE_CODE = 'SECURE_EXCHANGE_SCHOOL';
 
 UPDATE EDX_ROLE
-SET LABEL = 'Secure Messaging', DESCRIPTION = 'Can send and receive messages through the Secure Messaging Inbox.'
+SET LABEL = 'Secure Messaging', DESCRIPTION = 'Can access the district''s Secure Messaging Inbox.'
 WHERE EDX_ROLE_CODE = 'SECURE_EXCHANGE_DISTRICT';
+
+DELETE FROM EDX_ROLE_PERMISSION
+WHERE EDX_ROLE_CODE IN ('EDX_SCHOOL_ADMIN', 'EDX_DISTRICT_ADMIN')
+AND EDX_PERMISSION_CODE IN ('EDX_SCHOOL_EDIT', 'EDX_DISTRICT_EDIT', 'SECURE_EXCHANGE', 'STUDENT_DATA_COLLECTION');
