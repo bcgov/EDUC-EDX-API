8c4881c0cd6aae141165e941c07f6ee43f54d94b
Marco Villeneuve <arcshiftsolutions@gmail.com>
2024-07-08 10:29:51 -0700

    Updates for read only school and district perm

--- api/src/main/resources/db/migration/V1.0.47__EDX_API.sql
+++ api/src/main/resources/db/migration/V1.0.47__EDX_API.sql
@@ -1,8 +1,8 @@
 INSERT INTO EDX_PERMISSION (EDX_PERMISSION_CODE, LABEL, DESCRIPTION, CREATE_USER, UPDATE_USER)
-VALUES ('SCH_SDC_RO', 'School Student Data Collection Read-Only', 'Student Data Collection (1701) read-only permission for School.', 'IDIR/MVILLENE', 'IDIR/MVILLENE');
+VALUES ('SCH_SDC_RO', 'School SDC Read-Only', 'Student Data Collection (1701) read-only permission for School.', 'IDIR/MVILLENE', 'IDIR/MVILLENE');
 
 INSERT INTO EDX_PERMISSION (EDX_PERMISSION_CODE, LABEL, DESCRIPTION, CREATE_USER, UPDATE_USER)
-VALUES ('DIS_SDC_RO', 'District Student Data Collection Read-Only', 'Student Data Collection (1701) read-only permission for District.', 'IDIR/MVILLENE', 'IDIR/MVILLENE');
+VALUES ('DIS_SDC_RO', 'District SDC Read-Only', 'Student Data Collection (1701) read-only permission for District.', 'IDIR/MVILLENE', 'IDIR/MVILLENE');
 
 INSERT INTO EDX_ROLE_PERMISSION (EDX_ROLE_PERMISSION_ID, EDX_ROLE_CODE, EDX_PERMISSION_CODE, CREATE_USER, CREATE_DATE,
                                  UPDATE_USER, UPDATE_DATE)
