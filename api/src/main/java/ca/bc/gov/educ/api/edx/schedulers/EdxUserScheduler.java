f3e6eb0f8be7a573def8a60812bed8d743d7d2b4
Marco Villeneuve <arcshiftsolutions@gmail.com>
2025-03-21 11:02:35 -0700

    Small fix for purge cron value

--- api/src/main/java/ca/bc/gov/educ/api/edx/schedulers/EdxUserScheduler.java
+++ api/src/main/java/ca/bc/gov/educ/api/edx/schedulers/EdxUserScheduler.java
@@ -3,6 +3,7 @@ package ca.bc.gov.educ.api.edx.schedulers;
 import ca.bc.gov.educ.api.edx.model.v1.EdxUserDistrictEntity;
 import ca.bc.gov.educ.api.edx.model.v1.EdxUserSchoolEntity;
 import ca.bc.gov.educ.api.edx.repository.*;
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockAssert;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
+@Slf4j
 public class EdxUserScheduler {
   private final EdxUserSchoolRepository edxUserSchoolRepository;
   private final EdxUserDistrictRepository edxUserDistrictRepository;
@@ -31,11 +33,16 @@ public class EdxUserScheduler {
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void purgeExpiredEdxUsers() {
     LockAssert.assertLocked();
+
     final LocalDateTime now = LocalDateTime.now();
     List<EdxUserSchoolEntity> expiredSchoolUsers = this.edxUserSchoolRepository.findAllByExpiryDateBefore(now);
     List<EdxUserDistrictEntity> expiredDistrictUsers = this.edxUserDistrictRepository.findAllByExpiryDateBefore(now);
 
+    log.info("Purging expired school users");
+    expiredSchoolUsers.forEach(edxUserSchoolEntity -> log.info("Removing EDX user " + edxUserSchoolEntity.getEdxUserEntity().getEdxUserID() + " from school ID: " + edxUserSchoolEntity.getSchoolID()));
     this.edxUserSchoolRepository.deleteAll(expiredSchoolUsers);
+    log.info("Purging expired district users");
+    expiredDistrictUsers.forEach(edxUserDistrictEntity -> log.info("Removing EDX user " + edxUserDistrictEntity.getEdxUserEntity().getEdxUserID() + " from district ID: " + edxUserDistrictEntity.getDistrictID()));
     this.edxUserDistrictRepository.deleteAll(expiredDistrictUsers);
   }
 }
